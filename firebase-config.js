9b1279b99084991698a0a3b661ba5b3bb0090fb5
heracare <heradigital.id@gmail.com>
2026-05-30 19:51:30 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyACKcwiYqpEHK63ayypNhob1FLVvPKrcsU",
-  authDomain: "hera-727d4.firebaseapp.com",
-  projectId: "hera-727d4",
-  storageBucket: "hera-727d4.firebasestorage.app",
-  messagingSenderId: "951653859422"
+  apiKey: "AIzaSyAzvqhCE6Vw2I0jgGVJb3VUPCDPMTT9R6E",
+  authDomain: "hera-e4a7c.firebaseapp.com",
+  projectId: "hera-e4a7c",
+  storageBucket: "hera-e4a7c.firebasestorage.app",
+  messagingSenderId: "1078325065239"
 };
 
 // Expose globally for app.js to access
